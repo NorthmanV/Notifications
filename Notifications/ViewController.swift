452801b50021593737473ca6af7a55fbfdd98650
Ruslan Akberov <ruslanakberov@gmail.com>
2017-11-19 23:25:55 +0300

    Simple message received

--- Notifications/ViewController.swift
+++ Notifications/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error?.localizedDescription ?? "Error")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyButtonPressed(_ sender: UIButton) {
+        sheduleNotification(inSeconds: 5) { success in
+            if success {
+                print("Success notification!")
+            } else {
+                print("Error in notifications!")
+            }
+        }
     }
-
+    
+    func sheduleNotification(inSeconds: TimeInterval, complition: @escaping (_ success: Bool) -> ()) {
+        let notif = UNMutableNotificationContent()
+        notif.title = "New Notification"
+        notif.subtitle = "Good News!"
+        notif.body = "Notification is recivied. That is awesome! Let's try to do something new."
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        let request = UNNotificationRequest(identifier: "myNotif", content: notif, trigger: notifTrigger)
+        UNUserNotificationCenter.current().add(request) { error in
+            if error != nil {
+                print(error.debugDescription)
+                complition(false)
+            } else {
+                complition(true)
+            }
+        }
+    }
+    
 
 }
+
+
+
+
+
+
+
+
 
